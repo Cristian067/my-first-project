4203c7eede8fe94d46eb0a033f3b9a062aba5a8b
Cristian067 <cristian.n.c005@gmail.com>
2023-10-16 09:51:32 +0200

    v.0.8.27

--- Assets/Scripts/extra1/extra1_2.cs
+++ Assets/Scripts/extra1/extra1_2.cs
@@ -1,14 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class extra1_2 : MonoBehaviour
 {
+
+    [SerializeField] private int dia;
+    [SerializeField] private int mes;
+    private int tDias = 30;
+    private int tMes = 12;
+
+    
+    [SerializeField] private int alt;
+    [SerializeField] private int anch;
+
+
+    [SerializeField] private int actYear;
+    [SerializeField] private int nacYear;
+
+    //ej.16
+    [SerializeField] private string p;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ej.10
+        Debug.Log("Han pasado " + (dia + (mes * 30)) + " dias");
+
+
+        //ej.13
+
+        Debug.Log((alt + anch) / 2);
+
+
+        //ej.14
+
+        Debug.Log(-(nacYear - actYear)+ " años");
+
+        //ej.15
+
+
+
     }
+
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/sin_conexion/scripts/script_1.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_1 : MonoBehaviour
+{
+
+    /*
+    private bool monday;
+    private bool tuesday;
+    private bool wednesday;
+    private bool thursday;
+    private bool friday;
+    private bool saturday;
+    */
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      /*  
+        if (monday)
+        {
+            Debug.Log("Lunes");
+        }
+        else if (tuesday) 
+        {
+            Debug.Log("Martes");
+        }
+        else if (wednesday)
+        {
+            Debug.Log("Miercoles");
+        }
+        else if (thursday)
+        {
+            Debug.Log("Jueves");
+        }
+        else if (friday)
+        {
+            Debug.Log("Viernes");
+        }
+        else if (saturday) 
+        {
+            Debug.Log("Sabado");
+        }
+        else
+        {
+            Debug.Log("Domingo");
+        }
+      */
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
