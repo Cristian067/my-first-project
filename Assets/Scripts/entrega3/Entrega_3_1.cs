c1ee9b7aa218ba024741158979055c3c948a38e1
Cristian067 <cristian.n.c005@gmail.com>
2023-10-23 20:28:35 +0200

    V0.8.6

--- Assets/Scripts/entrega3/Entrega_3_1.cs
+++ Assets/Scripts/entrega3/Entrega_3_1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Entrega_3 : MonoBehaviour
+public class Entrega_3_1 : MonoBehaviour
 {
 
 
@@ -30,10 +30,6 @@ public class Entrega_3 : MonoBehaviour
     [SerializeField] private string[] chino = new string[12];
 
     [SerializeField] private string[] horoscopo = new string[12];
-
-    [SerializeField] private int t1;
-    [SerializeField] private int t2;
-    [SerializeField] private int t3;
 
     private int numhorosc;
 
@@ -90,7 +86,7 @@ public class Entrega_3 : MonoBehaviour
         {
             Debug.Log("C = aprobado");
         }
-        
+
         else if (grade < 5)
         {
             Debug.Log("D = suspendido");
@@ -98,7 +94,7 @@ public class Entrega_3 : MonoBehaviour
 
         //ej.5
 
-        if (day <=30 && month <= 12 && year > 0)
+        if (day <= 30 && month <= 12 && year > 0)
         {
             Debug.Log($"La fecha {day}/{month}/{year} es valida");
         }
@@ -119,61 +115,7 @@ public class Entrega_3 : MonoBehaviour
 
         //ej.7
 
-        
-
-
-        if(month == 3 && day >= 21 || month == 4 && day <= 19)
-        {
-            numhorosc = 0;
-            
-        }
-        
-        else if (month == 4 && day >= 20 || month == 5 && day <= 20)
-        {
-            numhorosc = 1;
-        }
-        else if (month == 5 && day >= 21 || month == 6 && day <= 20)
-        {
-            numhorosc = 2;
-        }
-        else if (month == 6 && day >= 21 || month == 7 && day <= 22)
-        {
-            numhorosc = 3;
-        }
-        else if (month == 7 && day >= 23 || month == 8 && day <= 22)
-        {
-            numhorosc = 4;
-        }
-        else if (month == 8 && day >= 23 || month == 9 && day <= 22)
-        {
-            numhorosc = 5;
-        }
-        else if (month == 9 && day >= 23 || month == 10 && day <= 22)
-        {
-            numhorosc = 6;
-        }
-        else if (month == 10 && day >= 23 || month == 11 && day <= 22)
-        {
-            numhorosc = 7;
-        }
-        else if (month == 11 && day >= 23 || month == 12 && day <= 21)
-        {
-            numhorosc = 8;
-        }
-        else if (month == 12 && day >= 22 || month == 1 && day <= 19)
-        {
-            numhorosc = 9;
-        }
-        else if (month == 1 && day >= 20 || month == 2 && day <= 18)
-        {
-            numhorosc = 10;
-        }
-        else if (month == 2 && day >= 19 ||  month == 3 && day <= 20)
-        {
-            numhorosc = 11;
-        }
-
-
+        Horoscopio();
         Debug.Log(horoscopo[numhorosc]);
 
 
@@ -194,11 +136,11 @@ public class Entrega_3 : MonoBehaviour
         {
             Debug.Log("75% de descuento");
         }
-        else if(age >=65 && rank == "silver")
+        else if (age >= 65 && rank == "silver")
         {
             Debug.Log("50% de descuento");
         }
-        else if (age >=65 && rank == "bronze")
+        else if (age >= 65 && rank == "bronze")
         {
             Debug.Log("25% de descuento");
         }
@@ -218,22 +160,14 @@ public class Entrega_3 : MonoBehaviour
         {
             Debug.Log("15% de descuento");
         }
-        
 
 
-        //ej.10
-        if(t1 + t2 + t3 == 180)
-        {
-            Debug.Log($"Los angulos {t1}, {t2} y {t3} pueden formar un triangulo");
-        }
-        else
-        {
-            Debug.Log($"Los angulos {t1}, {t2} y {t3} no pueden formar un triangulo");
-        }
 
-        
 
-        
+
+
+
+
 
 
 
@@ -244,11 +178,18 @@ public class Entrega_3 : MonoBehaviour
 
     }
 
-    
+    void Horoscopio()
+    {
+        if (month == 3 && day >= 21 || month == 4 && day <= 19)
+        {
+            numhorosc = 0;
+            //return numhorosc;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
