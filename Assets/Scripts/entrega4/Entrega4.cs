aeecbc8b9a0bf8d95ffd0c7b54deb632e0e9e160
Cristian067 <cristian.n.c005@gmail.com>
2023-10-29 16:05:42 +0100

    V0.8.7

--- Assets/Scripts/entrega4/Entrega4.cs
+++ Assets/Scripts/entrega4/Entrega4.cs
@@ -14,6 +14,14 @@ public class Entrega4 : MonoBehaviour
     [SerializeField] private int month;
     [SerializeField] private int year;
 
+    [SerializeField] private int tBase;
+    [SerializeField] private int tHeight;
+
+    [SerializeField] private int note;
+
+    [SerializeField] private int num3;
+
+
     private int actYear = System.DateTime.Now.Year;
 
 
@@ -28,7 +36,20 @@ public class Entrega4 : MonoBehaviour
 
         Debug.Log(vowel(letter));
 
-        Debug.Log(validDate(day, month, year, actYear)); 
+        Debug.Log(validDate(day, month, year, actYear));
+
+        Debug.Log(biYear(year));
+
+        oneMoreDay(day,month,year);
+        oneLessDay(day,month,year);
+
+        Debug.Log(chino(year));
+
+        Debug.Log(tArea(tBase, tHeight));
+
+        Debug.Log(grade(note));
+
+        Debug.Log(isEven(num3));
 
     }
 
@@ -92,6 +113,179 @@ public class Entrega4 : MonoBehaviour
     }
 
 
+    //ej.4
+
+    private string biYear(int year)
+    {
+        bool boolBi = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+        string bi ="";
+
+        if (boolBi)
+        {
+            bi = $"El año {year} es bisiesto";
+        }
+        else
+        {
+            bi = $"El año {year} no es bisiesto";
+        }
+        return bi;
+    }
+
+    //ej.5
+
+    private void oneMoreDay(int day, int month, int year)
+    {
+        day ++;
+
+        if(month == 2 && day > 28)
+        {
+            month ++;
+            day = day - 28;
+        }
+
+        if(day > 31)
+        {
+            day = day - 31;
+            month++;
+        }
+        if (month > 12)
+        {
+            month = month - 12;
+            year++;
+        }
+        Debug.Log($"La fecha con un dia mas es {day}/{month}/{year}");
+    }
+
+    //ej.6
+    private void oneLessDay(int day, int month, int year)
+    {
+        day--;
+
+        if (month == 2 && day > 28)
+        {
+            month++;
+            day = day - 28;
+        }
+
+        if (day > 31)
+        {
+            day = day - 31;
+            month++;
+        }
+        if (month > 12)
+        {
+            month = month - 12;
+            year++;
+        }
+
+        if (month == 3 && day < 1)
+        {
+            month++;
+            day = day + 28;
+        }
+
+        if (day < 1)
+        {
+            day = day + 31;
+            month--;
+        }
+        if (month < 1)
+        {
+            month = month + 12;
+            year--;
+        }
+        Debug.Log($"La fecha con un dia menos es {day}/{month}/{year}");
+    }
+
+    //ej.7
+
+    private string chino(int year)
+    {
+        string[] xino = new string[12];
+        xino[0] = "Rata";
+        xino[1] = "Buey";
+        xino[2] = "Tigre";
+        xino[3] = "Conejo";
+        xino[4] = "Dragon";
+        xino[5] = "Serpiente";
+        xino[6] = "Caballo";
+        xino[7] = "Cabra";
+        xino[8] = "Mono";
+        xino[9] = "Gallo";
+        xino[10] = "Perro";
+        xino[11] = "Cerdo";
+
+
+        int animal = year % 12 - 4;
+
+        string result = $"El año chino del año {year} es {xino[animal]}";
+
+        return result ;
+    }
+
+    //ej.8
+
+    private int tArea(int tBase, int tHeight)
+    {
+
+        int area = 0;
+        if (tBase >= 1 && tHeight >= 1)
+        {
+
+        area = (tBase * tHeight) / 2;
+        }
+        return area;
+
+    }
+
+    //ej.9
+
+    private string grade(int note)
+    {
+        string result = "";
+        if(note < 5)
+        {
+            result = "D"; 
+        }
+        else if(note >= 5 && note < 7)
+        {
+            result = "C";
+        }
+        else if(note >= 7 && note < 9)
+        {
+            result = "B";
+        }
+        else if(note >= 9)
+        {
+            result = "A";
+        }
+
+        return result ;
+
+    }
+
+
+    //ej.10
+    private bool isEven(int num)
+    {
+        bool result = false;
+
+        int Even = 0;
+
+        Even = num % 2;
+
+        if(Even == 0)
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+
+        return result;
+
+    }
 
 
 
